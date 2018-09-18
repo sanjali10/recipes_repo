e030dbd9cb9f4bc9b7b87c2c6cd9484fa50d6e6c
Anjali Sharma <sanjali@ssdiblr.sharp.co.in>
2018-09-18 15:16:16 +0530

    working on recipe and shoppinglist

--- projectPlaning/src/app/recipes/receipes-list/receipes-list.component.ts
+++ projectPlaning/src/app/recipes/receipes-list/receipes-list.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Receipe } from '../receipe.model'
+import {  Receipe } from '../receipe.model'
 @Component({
   selector: 'app-receipes-list',
   templateUrl: './receipes-list.component.html',
   styleUrls: ['./receipes-list.component.css']
 })
 export class ReceipesListComponent implements OnInit {
-  recipes:Receipe[]=[];
-  constructor() { }
+  recipes:Receipe[]=[
+    new Receipe('A Test Recipe','This is a simple test','http://www.trandynow.com/wp-content/uploads/2018/04/recipe.jpg')
+  ];
+  constructor() {
+   
+   }
 
   ngOnInit() {
   }
--- projectPlaning/src/app/shopping-list/shopping-list.component.ts
+++ projectPlaning/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredients } from '../shared/ingredients.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredients[]=[
+    new Ingredients('Apple',5),
+    new Ingredients('Tomotoes',10),
+  ];
   constructor() { }
 
   ngOnInit() {
