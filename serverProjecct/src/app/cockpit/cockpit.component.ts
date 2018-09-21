9d8f2ea5be1220daecab208120a15f1dcf3160be
Anjali Sharma <sanjali@ssdiblr.sharp.co.in>
2018-09-21 10:15:04 +0530

    serverProject_updates

--- serverProjecct/src/app/cockpit/cockpit.component.ts
+++ serverProjecct/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,24 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class CockpitComponent implements OnInit {
  newServerName='';
  newServerContent='';
+@Output() serverCreated=new EventEmitter<{serverName: string; serverContent: string}>();
+ @Output()blueprintCreated=new EventEmitter<{serverName: string; serverContent: string}>();
   constructor() { }
 
   ngOnInit() {
   }
-  //onAddServer()
- // {
-   // this.serverElements.push({
-  //    type:'server',
-    //  name:this.newserverName,
-   //   content:this.newServerContent
-  //  });
-  //}
- // onAddBlueprint(){
-  //  this.serverElement.push({
-  //    type:'blueprint',
-  //    name:this.newserverName,
-  //    content:this.newServerContent
-  //  });
-//  }
+  onAddServer()
+  {
+    this.serverCreated.emit({
+      serverName:this.newServerName,
+      serverContent:this.newServerContent
+    });
+  }
+ onAddBlueprint()
+ {
+   this.blueprintCreated.emit({
+    serverName:this.newServerName,
+    serverContent:this.newServerContent
+   });
+  
+ }
 
 }
